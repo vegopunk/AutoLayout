0d9c3752f3310c7a0d8af7fab44123c5983b5e7b
vegopunk <vegopunk@mail.ru>
2017-11-25 21:31:30 +0300

    Реализован свайп экранов ep.4

---
+++ AutoLayout/PageCell.swift
@@ -0,0 +1,77 @@
+//
+//  PageCell.swift
+//  AutoLayout
+//
+//  Created by Денис Попов on 25.11.2017.
+//  Copyright © 2017 Денис Попов. All rights reserved.
+//
+
+import UIKit
+
+class PageCell: UICollectionViewCell {
+    
+    let bearImageView : UIImageView = {
+        let imageView = UIImageView(image: #imageLiteral(resourceName: "bear_first"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    let decriptionTextView : UITextView = {
+        let textView = UITextView()
+        //        textView.text = "Join us today in our fun and games!"
+        //        textView.font = UIFont.boldSystemFont(ofSize: 18)
+        
+        let attributedText = NSMutableAttributedString(string: "Join us today in our fun and games!", attributes: [NSAttributedStringKey.font: UIFont.boldSystemFont(ofSize: 18)])
+        
+        attributedText.append(NSAttributedString(string: "\n\n\nAre you ready for loads and loads of fun? Don't wait any longer! We hope to see you in our stores soon.", attributes: [NSAttributedStringKey.font: UIFont.systemFont(ofSize: 13), NSAttributedStringKey.foregroundColor : UIColor.gray]))
+        
+        textView.attributedText = attributedText
+        
+        textView.textAlignment = .center
+        textView.isEditable = false
+        textView.isScrollEnabled = false
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        return textView
+    }()
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupLayout()
+    }
+    
+    private func setupLayout() {
+        
+        let topImageContainerView = UIView()
+        //        topImageContainerView.backgroundColor = .blue
+        addSubview(topImageContainerView)
+        //        topImageContainerView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        topImageContainerView.translatesAutoresizingMaskIntoConstraints = false
+        topImageContainerView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        
+        topImageContainerView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        topImageContainerView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        
+        //        topImageContainerView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        //        topImageContainerView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        topImageContainerView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.5).isActive = true
+        
+        topImageContainerView.addSubview(bearImageView)
+        bearImageView.centerXAnchor.constraint(equalTo: topImageContainerView.centerXAnchor).isActive = true
+        bearImageView.centerYAnchor.constraint(equalTo: topImageContainerView.centerYAnchor).isActive = true
+        bearImageView.heightAnchor.constraint(lessThanOrEqualTo: topImageContainerView.heightAnchor, multiplier: 0.5).isActive = true
+        
+        addSubview(decriptionTextView)
+        decriptionTextView.topAnchor.constraint(equalTo: topImageContainerView.bottomAnchor).isActive = true
+        decriptionTextView.leftAnchor.constraint(equalTo: leftAnchor, constant: 24).isActive = true
+        decriptionTextView.rightAnchor.constraint(equalTo: rightAnchor, constant: -24).isActive = true
+        decriptionTextView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: 0).isActive = true
+    }
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ AutoLayout/SwipingController.swift
@@ -0,0 +1,41 @@
+//
+//  SwippingController.swift
+//  AutoLayout
+//
+//  Created by Денис Попов on 25.11.2017.
+//  Copyright © 2017 Денис Попов. All rights reserved.
+//
+
+import UIKit
+
+class SwipingController: UICollectionViewController,UICollectionViewDelegateFlowLayout {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView?.register(PageCell.self, forCellWithReuseIdentifier: "cellID")
+        collectionView?.backgroundColor = .white 
+        
+        collectionView?.isPagingEnabled = true
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellID", for: indexPath)
+//        cell.backgroundColor = indexPath.item % 2 ==  0 ? .red : .green
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: view.frame.width , height: view.frame.height)
+    }
+    
+}
